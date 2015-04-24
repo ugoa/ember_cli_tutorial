d12ff802b81f176502b814a4b249e33fb4c436ea
David.H <david.hu@tinkerbox.com.sg>
2015-04-24 12:04:00 +0800

    Indicate how many todos left undone

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -10,12 +10,22 @@ export default Ember.ArrayController.extend({
 
       var todo_item = this.store.createRecord('todo', {
         title: title,
-          isCompleted: false
+        isCompleted: false
       });
 
       this.set('newTitle', '');
       
       todo_item.save;
     }
-  }
+  },
+
+  remaining: function () {
+    return this.filterBy('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function() {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining')
+
 });
