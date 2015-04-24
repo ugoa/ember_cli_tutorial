1da28eaf8847b6133cdfca01bb19780220654e21
David.H <david.hu@tinkerbox.com.sg>
2015-04-24 11:52:02 +0800

    Mark completed todos

---
+++ app/controllers/todo.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  isCompleted: function(key, value) {
+    var model = this.get('model');
+    if (value === undefined) {
+      return model.get('isCompleted');
+    } else {
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted')
+});
